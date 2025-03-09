08d659ec1f2bee9e62a26779b6f82ad901153fb5
Alif Alfanza <alifalfanza.anantarupa@gmail.com>
2025-03-09 13:23:42 +0700

    Fix knex config

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,21 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
       database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
+      user: process.env.DB_USER,
       password: process.env.DB_PASS,
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT
+      port: process.env.DB_PORT,
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
